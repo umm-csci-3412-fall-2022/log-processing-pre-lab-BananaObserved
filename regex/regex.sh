86c95be64f221ae35d948d758a9c199aab438811
Banana Observed <golds349@morris.umn.edu>
2022-09-13 16:07:53 -0500

    Create and use functioning regex
    Add in a singular regex which matches all patterns for all files in the tests.  Passes all tests,
    and passes shellcheck when the bash or sh shell is specified.

--- regex/regex.sh
+++ regex/regex.sh
@@ -1,3 +1,7 @@
-full_regex="(?<=.+?)((?:(?<= )\w+(?=[.,] ))|(?:\w\.){2,})\s*(?:My favorite sandwich is)?\s*(.*?)\.?$"
+run_regex(){
+	gawk 'match($0, /((\<[A-Z]\w+)|(\<(\w+\.)+))[.,]? (My favorite sandwich is )?([^.,]*)\.?$/, groups) {print "1. " groups[1] "\n2. " groups[6] "\n"}' "$1_input.txt" > "$1_output.txt"
+}
 
-
+run_regex "r0"
+run_regex "r1"
+run_regex "r2"
